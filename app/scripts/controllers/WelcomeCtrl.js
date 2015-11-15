208bfa02486b03cdc4d1377385c8dcaeab07b977
Shankar Natarajan <shankarvn@gmail.com>
2015-11-14 19:55:03 -0800

    Using browserify to build angular app in a node like fashion:

---
+++ app/scripts/controllers/WelcomeCtrl.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var WelcomeCtrl = function($scope) {
+  $scope.testVar = 'We are up and running from a required module!. And it reloads';
+};
+
+module.exports = WelcomeCtrl;
---
+++ readme.txt
@@ -0,0 +1,1 @@
+This is a sample project that will use Node in the backend and Angularjs 2.0 in the front end.
